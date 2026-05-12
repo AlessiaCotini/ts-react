fdc2b5eb855f9eba4ba0acf933014a49e8775340
AlessiaCotini <alessia.cotini@icloud.com>
2026-05-12 13:37:42 +0200

    start newspaper

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
-import ClassComponent from "./components/ClassComponent";
-import FunctionalComponent from "./components/FunctionalComponent";
-import Fetchcomponents from "./components/Fetchcomponent";
+
 function App() {
   return (
     <>
       <Container fluid>
         <Row className="justify-content-center">
-          <Col xs={12} md={6} lg={4}>
-            <Fetchcomponents />
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col xs={12} md={6} lg={4}>
-            <ClassComponent content="Stiamo passando delle props al componenete" />
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col>
-            <FunctionalComponent content="passiamo props anche a questo" />
-          </Col>
+          <Col xs={12} md={6} lg={4}></Col>
         </Row>
       </Container>
     </>
